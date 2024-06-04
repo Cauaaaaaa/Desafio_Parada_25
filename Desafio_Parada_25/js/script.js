d7d6e1bc5033e77ad7e3681b1bd94446ba69e206
CauãZão <163067312+Cauaaaaaa@users.noreply.github.com>
2024-06-03 23:59:19 -0300

    Add files via upload

---
+++ Desafio_Parada_25/js/script.js
@@ -0,0 +1,23 @@
+let convidados = [];
+
+    let ul = document.getElementById("convite");
+
+function colocarNome(){
+    let p = document.getElementById("nomes").value;
+    convidados.push(p)
+                
+    document.getElementById("nomes").value = "";
+    colocar_nomes();
+}
+
+function tirarNome(){
+    convidados.pop();
+    colocar_nomes();
+}
+
+function colocar_nomes(){
+    ul.innerHTML = "";
+    for(let x=0; x < convidados.length; x++){
+        ul.innerHTML += `<li> ${convidados[x]} </li>`;
+    }
+}
